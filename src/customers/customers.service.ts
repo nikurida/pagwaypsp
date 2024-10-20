e6af745cf48fcaa4d1c0d0973af760922a2d08b9
denis-wilton <deniswiltonpa@gmail.com>
2024-10-19 22:19:06 -0400

    Refactor

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CustomersDto } from './dto/customers.dto';
 import * as bcrypt from 'bcrypt';
 import { EntityManager, Repository } from 'typeorm';
-import { Customers as CustomersEntity } from './entitites/customers.entity';
+import { Customers as Customers } from './entitites/customers.entity';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 
 type Identity<T> = T extends object ? { [K in keyof T]: T[K] } : T;
@@ -13,40 +13,20 @@ export class CustomerService {
   private readonly logger = new Logger(CustomerService.name);
 
   constructor(
-    @InjectRepository(CustomersEntity)
-    private customerRepository: Repository<CustomersEntity>,
+    @InjectRepository(Customers)
+    private repo: Repository<Customers>,
     @InjectEntityManager()
-    private entityManager: EntityManager,
+    private mngr: EntityManager,
   ) {}
 
   async create(customerDto: CustomersDto): Promise<Customer> {
-    const buildedCustomer = await this.buildCustomer({
-      ...customerDto,
-    });
-
     try {
-      const customer = await this.entityManager.transaction(
-        async (entityManager) => {
-          const savedCustomer = await entityManager.save(
-            CustomersEntity,
-            buildedCustomer,
-          );
-          return savedCustomer;
-        },
+      const entity = this.repo.create(customerDto);
+      return await this.mngr.transaction(
+        async (mngr) => await mngr.save(Customers, entity),
       );
-
-      return customer;
-    } catch (e) {
-      this.logger.error(e);
-    }
-  }
-
-  private async buildCustomer(customer: CustomersDto) {
-    try {
-      const buildedCustomer = await this.customerRepository.create(customer);
-      return buildedCustomer;
-    } catch (e) {
-      this.logger.error(e);
+    } catch (err) {
+      this.logger.error(err);
     }
   }
 }
