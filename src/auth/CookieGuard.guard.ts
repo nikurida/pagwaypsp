ea8e48351af6bb746f069b97ce4207c464defd5c
denis-wilton <deniswiltonpa@gmail.com>
2024-10-28 01:50:18 -0400

    Some changes

---
+++ src/auth/CookieGuard.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CookieGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const isCookiesEnabled = request.cookies;
+    if (!isCookiesEnabled) {
+      return false;
+    }
+
+    // check if jwt is in the cookie
+    const token: string | undefined = request.cookies['jwt'];
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      request.user = decoded;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
